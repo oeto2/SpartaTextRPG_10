1409f61b243fbbd6c4a53951aae80f1194d9769a
oeto2 <oeto2@naver.com>
2024-01-09 19:31:48 +0900

    0109_EditTitleView

--- State.cs
+++ State.cs
@@ -8,6 +8,6 @@ namespace SpartaTextRPG
 {
     public class State
     {
-
+        
     }
 }
--- Title.cs
+++ Title.cs
@@ -8,5 +8,24 @@ namespace SpartaTextRPG
 {
     internal class Title
     {
+        public static Title instance = new Title();
+
+
+        public void ShowTitle()
+        {
+            Console.Clear();
+            Console.WriteLine("  ____                   _          ____  ____   ____ ");
+            Console.WriteLine(" / ___| _ __   __ _ _ __| |_ __ _  |  _ \\|  _ \\ / ___|");
+            Console.WriteLine(" \\___ \\| '_ \\ / _` | '__| __/ _` | | |_) | |_) | |  _ ");
+            Console.WriteLine("  ___) | |_) | (_| | |  | || (_| | |  _ <|  __/| |_| |");
+            Console.WriteLine(" |____/| .__/ \\__,_|_|   \\__\\__,_| |_| \\_\\_|    \\____|");
+            Console.WriteLine("       |_|                                            ");
+            Console.WriteLine("============================================================");
+            Console.WriteLine("                 PRESS ANIKEY TO START                      ");
+            Console.WriteLine("============================================================");
+            Console.CursorVisible = false;
+            Console.ReadKey();
+            Console.CursorVisible = true;
+        }
     }
 }
