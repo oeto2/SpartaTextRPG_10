b9f7ca63739d8ee04f4881a1dc29b05d6ff4aaaa
sskesu <136444697+sskesu@users.noreply.github.com>
2024-01-10 17:34:17 +0900

    0110_Add Inven method
    Feat: Make inventory method

--- Inventory.cs
+++ Inventory.cs
@@ -1,19 +1,301 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace SpartaTextRPG
 {
+    enum InvenScene
+    {
+        NORMAL,
+        MANAGE,
+        ENFORCE
+    }
     internal class Inventory
     {
-        List<EquipItem> equipInven = new List<EquipItem>();
-        List<ConsumItem> consumInven = new List<ConsumItem>();
+        public static Inventory _instance;
 
+        public static Inventory Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Inventory();
+                }
+                return _instance;
+            }
+        }
+
+        InvenScene scene = InvenScene.NORMAL;
+
+        int row, col;
+
+        public List<int> ownEquipCount = new List<int>();
+        public List<int> ownConsumCount = new List<int>();
+
+        // 장비 장착 / 해제
+        void EquipOnOff(int index)
+        {
+            // 범위 초과 시 리턴
+            if (ownEquipCount.Count - 1 < index)
+            {
+                return;
+            }
+
+            switch (Item.Instance.equipItems[index].type)
+            {
+                case ItemType.WEAPON:
+                    if (Item.Instance.equipItems[index].id == Player.player.weapon)
+                    {
+                        Player.player.weapon = 0;
+                    }
+                    else
+                    {
+                        Player.player.weapon = Item.Instance.equipItems[index].id;
+                    }
+                    break;
+
+                case ItemType.ARMOR:
+                    if (Item.Instance.equipItems[index].id == Player.player.armor)
+                    {
+                        Player.player.armor = 0;
+                    }
+                    else
+                    {
+                        Player.player.armor = Item.Instance.equipItems[index].id;
+                    }
+                    break;
+
+                default:
+                    Console.WriteLine("장비 장착 / 해제 오류");
+                    break;
+            }
+        }
+
+        // 보유 장비 확인
+        public void CheckOwnEquip()
+        {
+            ownEquipCount.Clear();
+            for (int i = 0; i < Item.Instance.equipItems.Count; i++)
+            {
+                if (Item.Instance.equipItems[i].isOwn)
+                {
+                    ownEquipCount.Add(i);
+                }
+            }
+            ownConsumCount.Clear();
+            for (int i = 0; i < Item.Instance.consumItems.Count; i++)
+            {
+                if (Item.Instance.consumItems[i].count > 0)
+                {
+                    ownConsumCount.Add(i);
+                }
+            }
+        }
+
+        // 인벤토리 장비 리스트 출력
+        void ShowInvenEquipList()
+        {
+            Console.WriteLine("[아이템 목록]");
+            // 장비 목록
+            for (int i = 0; i < Item.Instance.equipItems.Count; i++)
+            {
+                if (Item.Instance.equipItems[i].isOwn)
+                {
+                    EquipItem equip = Item.Instance.equipItems[i];
+
+                    if (scene == InvenScene.NORMAL)
+                    {
+                        Console.Write("- ");
+                    }
+                    else if (scene != InvenScene.NORMAL)
+                    {
+                        Color.ChangeTextColor(Colors.MAGENTA, "", Convert.ToString(i + 1), " ");
+                    }
+
+                    if (Player.player.weapon == equip.id)
+                    {
+                        Color.ChangeTextColor(Colors.MAGENTA, "[", "E", "]");
+                    }
+                    Console.Write(equip.name);
+
+                    (row, col) = Console.GetCursorPosition();
+                    Console.SetCursorPosition(20, col);
+
+                    Console.Write("| ");
+
+                    if (equip.atk != 0)
+                    {
+                        Console.SetCursorPosition(22, col);
+                        Color.ChangeTextColor(Colors.MAGENTA, "공격력 +", Convert.ToString(equip.atk), " ");
+                    }
+                    if (equip.def != 0)
+                    {
+                        Console.SetCursorPosition(36, col);
+                        Color.ChangeTextColor(Colors.MAGENTA, "방어력 +", Convert.ToString(equip.def), " ");
+                    }
+
+                    Console.SetCursorPosition(50, col);
+                    Console.Write("| ");
+                    Console.WriteLine(equip.info);
+                }
+                
+            }
+            
+            
+        }
+        // 인벤토리 소비 리스트 출력
+        void ShowInvenConsumList()
+        {
+            // 소비 목록
+            for (int i = 0; i < Item.Instance.consumItems.Count; i++)
+            {
+                if (Item.Instance.consumItems[i].count > 0)
+                {
+                    ConsumItem consum = Item.Instance.consumItems[i];
+
+                    if (scene == InvenScene.NORMAL)
+                    {
+                        Console.Write("- ");
+                    }
+                    else if (scene == InvenScene.MANAGE)
+                    {
+                        Color.ChangeTextColor(Colors.MAGENTA, "", Convert.ToString(i + 1 + Item.Instance.equipItems.Count), " ");
+                    }
+                    Console.Write(consum.name);
+
+                    (row, col) = Console.GetCursorPosition();
+                    Console.SetCursorPosition(20, col);
+
+                    Console.Write("| ");
+
+                    if (consum.recoveryHp != 0)
+                    {
+                        Console.SetCursorPosition(22, col);
+                        Color.ChangeTextColor(Colors.MAGENTA, "HP회복량 +", Convert.ToString(consum.recoveryHp), " ");
+                    }
+                    if (consum.recoveryMp != 0)
+                    {
+                        Console.SetCursorPosition(36, col);
+                        Color.ChangeTextColor(Colors.MAGENTA, "MP회복량 +", Convert.ToString(consum.recoveryMp), " ");
+                    }
+
+                    Console.SetCursorPosition(50, col);
+                    Console.Write("| ");
+                    Console.Write(consum.info);
+
+                    Console.SetCursorPosition(80, col);
+                    Color.ChangeTextColor(Colors.MAGENTA, "| ", Convert.ToString(consum.count), "개 소지\n");
+                }
+            }
+        }
         public void ShowInvenPage()
         {
-            Color.ChangeTextColor(Colors.YELLOW, "", "인벤토리", "\n");
+            // 기본 페이지
+            if (scene == InvenScene.NORMAL)
+            {
+                Color.ChangeTextColor(Colors.YELLOW, "", "인벤토리", "\n");
+                Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.\n");
+
+                ShowInvenEquipList();
+                ShowInvenConsumList();
+
+                Console.WriteLine();
+                Color.ChangeTextColor(Colors.MAGENTA, "", "1", ". 장착 관리\n");
+                Color.ChangeTextColor(Colors.MAGENTA, "", "2", ". 장비 강화\n");
+                Color.ChangeTextColor(Colors.MAGENTA, "", "0", ". 나가기\n\n");
+
+                Console.WriteLine("원하시는 행동을 입력해주세요.");
+                Console.Write(">> ");
+
+                string answer = Console.ReadLine();
+                Console.Clear();
+
+                switch (answer)
+                {
+                    case "1":
+                        scene = InvenScene.MANAGE;
+                        break;
+                    case "2":
+                        scene = InvenScene.ENFORCE;
+                        break;
+                    case "0":
+                        Program.scene = Scene.mainScene;
+                        break;
+                    default:
+                        Color.ChangeTextColor(Colors.RED, "", "잘못된 입력입니다.", "\n");
+                        break;
+                }
+            }
+            // 장착 관리 페이지
+            else if (scene == InvenScene.MANAGE)
+            {
+                CheckOwnEquip();
+                Color.ChangeTextColor(Colors.YELLOW, "", "인벤토리 - 장착 관리", "\n");
+                Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.\n");
+
+                ShowInvenEquipList();
+
+                Console.WriteLine();
+                Color.ChangeTextColor(Colors.MAGENTA, "", "0", ". 나가기\n\n");
+
+                Console.WriteLine("원하시는 행동을 입력해주세요.");
+                Console.Write(">> ");
+
+                string answer = Console.ReadLine();
+                Console.Clear();
+
+                switch (answer)
+                {
+                    case "1":
+                    case "2":
+                    case "3":
+                    case "4":
+                    case "5":
+                    case "6":
+                    case "7":
+                        EquipOnOff(Convert.ToInt16(answer) - 1);
+                        break;
+                    case "0":
+                        scene = InvenScene.NORMAL;
+                        break;
+                    default:
+                        Color.ChangeTextColor(Colors.RED, "", "잘못된 입력입니다.", "\n");
+                        break;
+                }
+            }
+            // 장비 강화 페이지
+            else
+            {
+                CheckOwnEquip();
+                Color.ChangeTextColor(Colors.YELLOW, "", "인벤토리 - 장비 강화", "\n");
+                Console.WriteLine("보유 중인 아이템을 관리할 수 있습니다.\n");
+
+                ShowInvenEquipList();
+
+                Console.WriteLine("미구현입니다~");
+                Color.ChangeTextColor(Colors.MAGENTA, "", "0", ". 나가기\n\n");
+
+                Console.WriteLine("원하시는 행동을 입력해주세요.");
+                Console.Write(">> ");
+
+                string answer = Console.ReadLine();
+                Console.Clear();
+
+                switch (answer)
+                {
+                    case "0":
+                        scene = InvenScene.NORMAL;
+                        break;
+                    default:
+                        Color.ChangeTextColor(Colors.RED, "", "잘못된 입력입니다.", "\n");
+                        break;
+                }
+            }
+
         }
     }
 }
--- Shop.cs
+++ Shop.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SpartaTextRPG
 {
-    enum Scene
+    enum ShopScene
     {
         NORMAL,
         BUY,
@@ -29,7 +29,7 @@ namespace SpartaTextRPG
             }
         }
 
-        Scene scene = Scene.NORMAL;
+        ShopScene scene = ShopScene.NORMAL;
 
         int row, col;
 
@@ -41,11 +41,11 @@ namespace SpartaTextRPG
             {
                 EquipItem equip = Item.Instance.equipItems[i];
 
-                if (scene == Scene.NORMAL)
+                if (scene == ShopScene.NORMAL)
                 {
                     Console.Write("- ");
                 }
-                else if (scene == Scene.BUY)
+                else if (scene == ShopScene.BUY)
                 {
                     Color.ChangeTextColor(Colors.MAGENTA, "", Convert.ToString(i + 1), " ");
                 }
@@ -79,7 +79,7 @@ namespace SpartaTextRPG
                 }
                 else
                 {
-                    Color.ChangeTextColor(Colors.MAGENTA, "", Convert.ToString(equip.cost), "\n");
+                    Color.ChangeTextColor(Colors.MAGENTA, "", Convert.ToString(equip.cost), " G\n");
                 }
             }
 
@@ -87,11 +87,11 @@ namespace SpartaTextRPG
             {
                 ConsumItem consum = Item.Instance.consumItems[i];
 
-                if (scene == Scene.NORMAL)
+                if (scene == ShopScene.NORMAL)
                 {
                     Console.Write("- ");
                 }
-                else if (scene == Scene.BUY)
+                else if (scene == ShopScene.BUY)
                 {
                     Color.ChangeTextColor(Colors.MAGENTA, "", Convert.ToString(i + 1 + Item.Instance.equipItems.Count), " ");
                 }
@@ -119,7 +119,7 @@ namespace SpartaTextRPG
 
                 Console.SetCursorPosition(80, col);
                 Console.Write("| ");
-                Color.ChangeTextColor(Colors.MAGENTA, "", Convert.ToString(consum.cost), "\n");
+                Color.ChangeTextColor(Colors.MAGENTA, "", Convert.ToString(consum.cost), " G\n");
             }
         }
 
@@ -133,13 +133,13 @@ namespace SpartaTextRPG
                 {
                     Color.ChangeTextColor(Colors.BLUE, "", "이미 구매한 아이템입니다.", "\n");
                 }
-                else if (1000 < Item.Instance.equipItems[index].cost) // 돈 모자랄때 1000>>player.gold로
+                else if (Player.player.gold < Item.Instance.equipItems[index].cost)
                 {
                     Color.ChangeTextColor(Colors.RED, "", "Gold 가 부족합니다.", "\n");
                 }
                 else
                 {
-                    // 플레이어 골드 감소
+                    Player.player.gold -= Item.Instance.equipItems[index].cost;
                     Item.Instance.equipItems[index].isOwn = true;
                     Color.ChangeTextColor(Colors.BLUE, "", "구매를 완료했습니다.", "\n");
                 }
@@ -148,36 +148,173 @@ namespace SpartaTextRPG
             else
             {
                 int consumIndex = index - Item.Instance.equipItems.Count;
-                if (1000 < Item.Instance.consumItems[consumIndex].cost) // 돈 모자랄때 1000>>player.gold로
+                if (Player.player.gold < Item.Instance.consumItems[consumIndex].cost)
                 {
                     Color.ChangeTextColor(Colors.RED, "", "Gold 가 부족합니다.", "\n");
                 }
                 else
                 {
-                    // 플레이어 골드 감소 추가하기
+                    Player.player.gold -= Item.Instance.consumItems[consumIndex].cost;
                     Item.Instance.consumItems[consumIndex].count++;
+                    Color.ChangeTextColor(Colors.BLUE, "", "구매를 완료했습니다.", "\n");
                 }
-                           
+
+            }
+        }
+        // 판매 리스트 출력
+        void ShowSellList()
+        {
+            Inventory.Instance.CheckOwnEquip();
+
+            Console.WriteLine("[아이템 목록]");
+            // 장비 목록
+            for (int i = 0; i < Inventory.Instance.ownEquipCount.Count; i++)
+            {
+                int index = Inventory.Instance.ownEquipCount[i];
+
+                Color.ChangeTextColor(Colors.MAGENTA, "", Convert.ToString(i + 1), " ");
+
+                EquipItem equip = Item.Instance.equipItems[index];
+
+                if (Player.player.weapon == equip.id)
+                {
+                    Color.ChangeTextColor(Colors.MAGENTA, "[", "E", "]");
+                }
+                Console.Write(equip.name);
+
+                (row, col) = Console.GetCursorPosition();
+                Console.SetCursorPosition(20, col);
+
+                Console.Write("| ");
+
+                if (equip.atk != 0)
+                {
+                    Console.SetCursorPosition(22, col);
+                    Color.ChangeTextColor(Colors.MAGENTA, "공격력 +", Convert.ToString(equip.atk), " ");
+                }
+                if (equip.def != 0)
+                {
+                    Console.SetCursorPosition(36, col);
+                    Color.ChangeTextColor(Colors.MAGENTA, "방어력 +", Convert.ToString(equip.def), " ");
+                }
+
+                Console.SetCursorPosition(50, col);
+                Console.Write("| ");
+                Console.WriteLine(equip.info);
+
+                Console.SetCursorPosition(80, col);
+                Console.Write("| ");
+                
+                Color.ChangeTextColor(Colors.MAGENTA, "", Convert.ToString(equip.cost), " G\n");
+                
+            }
+            // 소비 목록
+            for (int i = 0; i < Item.Instance.consumItems.Count; i++)
+            {
+                if (Item.Instance.consumItems[i].count > 0)
+                {
+                    ConsumItem consum = Item.Instance.consumItems[i];
+
+                    Color.ChangeTextColor(Colors.MAGENTA, "", Convert.ToString(i + 1 + Inventory.Instance.ownEquipCount.Count), " ");
+
+                    Console.Write(consum.name);
+
+                    (row, col) = Console.GetCursorPosition();
+                    Console.SetCursorPosition(20, col);
+
+                    Console.Write("| ");
+
+                    if (consum.recoveryHp != 0)
+                    {
+                        Console.SetCursorPosition(22, col);
+                        Color.ChangeTextColor(Colors.MAGENTA, "HP회복량 +", Convert.ToString(consum.recoveryHp), " ");
+                    }
+                    if (consum.recoveryMp != 0)
+                    {
+                        Console.SetCursorPosition(36, col);
+                        Color.ChangeTextColor(Colors.MAGENTA, "MP회복량 +", Convert.ToString(consum.recoveryMp), " ");
+                    }
+
+                    Console.SetCursorPosition(50, col);
+                    Console.Write("| ");
+                    Console.Write(consum.info);
+
+                    Console.SetCursorPosition(80, col);
+                    Color.ChangeTextColor(Colors.MAGENTA, "| ", Convert.ToString(consum.count), "개 소지\n");
+
+                    Console.SetCursorPosition(94, col);
+                    Console.Write("| ");
+                    Color.ChangeTextColor(Colors.MAGENTA, "", Convert.ToString(consum.cost), " G\n");
+                }
             }
         }
 
+        // 판매 기능
+        void SellItems(int index)
+        {
+            // 장비 판매
+            if (Inventory.Instance.ownEquipCount.Count - 1 >= index)
+            {
+                int equip = Inventory.Instance.ownEquipCount[index];
+
+                switch (Item.Instance.equipItems[equip].type)
+                {
+                    case ItemType.WEAPON:
+                        if (Item.Instance.equipItems[equip].id == Player.player.weapon)
+                        {
+                            Player.player.weapon = 0;
+                        }
+                        Item.Instance.equipItems[equip].isOwn = false;
+                        Player.player.gold += Item.Instance.equipItems[equip].cost * 85 / 100;
+                        Color.ChangeTextColor(Colors.BLUE, "", Item.Instance.equipItems[equip].name + "을(를) 판매하였습니다.\n");
+                        break;
+
+                    case ItemType.ARMOR:
+                        if (Item.Instance.equipItems[equip].id == Player.player.armor)
+                        {
+                            Player.player.armor = 0;
+                        }
+                        Item.Instance.equipItems[equip].isOwn = false;
+                        Player.player.gold += Item.Instance.equipItems[equip].cost * 85 / 100;
+                        Color.ChangeTextColor(Colors.BLUE, "", Item.Instance.equipItems[equip].name + "을(를) 판매하였습니다.\n");
+                        break;
+
+                    default:
+                        Console.WriteLine("아이템 판매 기능 오류");
+                        break;
+                }
+            }
+            // 소모품 판매
+            else if (Inventory.Instance.ownEquipCount.Count + Inventory.Instance.ownConsumCount.Count - 1 >= index)
+            {
+
+            }
+            // 범위 초과 시 리턴
+            else
+            {
+                return;
+            }
+        }
         // 상점 출력
         public void ShowShopPage()
         {
-            if (scene == Scene.NORMAL)
+            if (scene == ShopScene.NORMAL)
             {
                 Color.ChangeTextColor(Colors.YELLOW, "", "상점", "\n");
                 Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.\n");
 
                 Console.WriteLine("보유 골드");
-                Color.ChangeTextColor(Colors.MAGENTA, "", Convert.ToString(10000), " G\n\n");
+                Color.ChangeTextColor(Colors.MAGENTA, "", Convert.ToString(Player.player.gold), " G\n\n");
 
                 ShopItemList();
 
-                Console.WriteLine("");
-                Console.WriteLine("1. 아이템 구매");
-                Console.WriteLine("2. 아이템 판매");
-                Console.WriteLine("0. 나가기\n");
+                Console.WriteLine();
+                Color.ChangeTextColor(Colors.MAGENTA, "", "1", ". 아이템 구매\n");
+                Color.ChangeTextColor(Colors.MAGENTA, "", "2", ". 아이템 판매\n");
+                Color.ChangeTextColor(Colors.MAGENTA, "", "0", ". 나가기\n\n");
+
+                Console.WriteLine("원하시는 행동을 입력해주세요.");
+                Console.Write(">> ");
 
                 string answer = Console.ReadLine();
                 Console.Clear();
@@ -185,31 +322,35 @@ namespace SpartaTextRPG
                 switch (answer)
                 {
                     case "1":
-                        scene = Scene.BUY;
+                        scene = ShopScene.BUY;
                         break;
                     case "2":
-                        scene = Scene.SELL;
+                        scene = ShopScene.SELL;
                         break;
                     case "0":
-                        // 메인화면으로
+                        Program.scene = Scene.mainScene;
                         break;
                     default:
                         Color.ChangeTextColor(Colors.RED, "", "잘못된 입력입니다.", "\n");
                         break;
                 }
             }
-            else if (scene == Scene.BUY)
+            // 구매 페이지
+            else if (scene == ShopScene.BUY)
             {
                 Color.ChangeTextColor(Colors.YELLOW, "", "상점 - 아이템 구매", "\n");
                 Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.\n");
 
                 Console.WriteLine("보유 골드");
-                Color.ChangeTextColor(Colors.MAGENTA, "", Convert.ToString(10000), " G\n\n");
+                Color.ChangeTextColor(Colors.MAGENTA, "", Convert.ToString(Player.player.gold), " G\n\n");
 
                 ShopItemList();
 
-                Console.WriteLine("");
-                Console.WriteLine("0. 나가기\n");
+                Console.WriteLine();
+                Color.ChangeTextColor(Colors.MAGENTA, "", "0", ". 나가기\n\n");
+
+                Console.WriteLine("원하시는 행동을 입력해주세요.");
+                Console.Write(">> ");
 
                 string answer = Console.ReadLine();
                 Console.Clear();
@@ -223,25 +364,64 @@ namespace SpartaTextRPG
                     case "5":
                     case "6":
                     case "7":
+                    case "8":
                         // 구매
                         BuyItems(Convert.ToInt16(answer) - 1);
                         break;
                     case "0":
-                        scene = Scene.NORMAL;
+                        scene = ShopScene.NORMAL;
                         break;
                     default:
                         Color.ChangeTextColor(Colors.RED, "", "잘못된 입력입니다.", "\n");
                         break;
                 }
             }
+            // 판매 페이지
             else
             {
-                // 판매 페이지
+                Color.ChangeTextColor(Colors.YELLOW, "", "상점 - 아이템 판매", "\n");
+                Console.WriteLine("필요한 아이템을 얻을 수 있는 상점입니다.\n");
+
+                Console.WriteLine("보유 골드");
+                Color.ChangeTextColor(Colors.MAGENTA, "", Convert.ToString(Player.player.gold), " G\n\n");
+
+                ShowSellList();
+
+                Console.WriteLine();
+                Color.ChangeTextColor(Colors.MAGENTA, "", "0", ". 나가기\n\n");
+
+                Console.WriteLine("원하시는 행동을 입력해주세요.");
+                Console.Write(">> ");
+
+                string answer = Console.ReadLine();
+                Console.Clear();
+
+                switch (answer)
+                {
+                    case "1":
+                    case "2":
+                    case "3":
+                    case "4":
+                    case "5":
+                    case "6":
+                    case "7":
+                    case "8":
+                    case "9":
+                        // 판매
+                        SellItems(Convert.ToInt16(answer) - 1);
+                        break;
+                    case "0":
+                        scene = ShopScene.NORMAL;
+                        break;
+                    default:
+                        Color.ChangeTextColor(Colors.RED, "", "잘못된 입력입니다.", "\n");
+                        break;
+                }
             }
 
         }
 
-        
+
     }
 
 }
