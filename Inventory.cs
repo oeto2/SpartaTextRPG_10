a1b61cdee5ec26837bdb93c798d7c20652e76fa5
sskesu <136444697+sskesu@users.noreply.github.com>
2024-01-10 11:08:56 +0900

    0110_AddShopPage
    Feat: Make shop page, show item list, change text color

---
+++ Inventory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpartaTextRPG
+{
+    internal class Inventory
+    {
+        List<EquipItem> equipInven = new List<EquipItem>();
+        List<ConsumItem> consumInven = new List<ConsumItem>();
+
+        public void ShowInvenPage()
+        {
+            Color.ChangeTextColor(Colors.YELLOW, "", "인벤토리", "\n");
+        }
+    }
+}
