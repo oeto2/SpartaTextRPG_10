3c35e1012b63902110c6a972635fbc82d375ff1d
oeto2 <oeto2@naver.com>
2024-01-11 10:29:09 +0900

    0110_Edit Show Quest List

--- DataClass/Quest.cs
+++ DataClass/Quest.cs
@@ -1,5 +1,8 @@
 ﻿using System.ComponentModel;
+using System.ComponentModel.Design;
+using System.Globalization;
 using System.Reflection;
+using System.Text;
 
 namespace SpartaTextRPG.DataClass
 {
@@ -31,18 +34,18 @@ namespace SpartaTextRPG.DataClass
         //메인 퀘스트 목록
         public static List<Quest> mainQuestList = new List<Quest>()
         {
-            new Quest(0,"1차 전직 하기","캐릭터의 레벨을 올려 1차 전직을 해보자.",2000, 200,true),
-            new Quest(2,"2차 전직 하기","캐릭터의 레벨을 올려 2차 전직을 해보자.",3000, 500,false)
+            new Quest(0,"1차 전직","캐릭터의 레벨을 올려 1차 전직을 해보자.",2000, 200,true),
+            new Quest(2,"2차 전직","캐릭터의 레벨을 올려 2차 전직을 해보자.",3000, 500,false)
         };
 
         //서브 퀘스트 목록
         public static List<Quest> subQuestList = new List<Quest>()
         {
-            new Quest(0,"아이템 장착하기","인벤토리에서 아이템을 장착해보자",500,50,true),
-            new Quest(1,"하급 던전 입장하기","하급 던전에 입장해보자",600,100, true),
-            new Quest(2,"휴식하기","상점에서 아이템을 구매해보자",500,50, true),
+            new Quest(0,"아이템 장착","인벤토리에서 아이템을 장착해보자",500,50,true),
+            new Quest(1,"하급 던전 입장","하급 던전에 입장해보자",600,100, true),
+            new Quest(2,"휴식","휴식 기능을 사용해보자",1000,150, true),
             new Quest(3,"하급 던전 클리어","하급 던전을 클리어해보자.",500,50, false),
-            new Quest(4,"포션 아이템 사용하기","상점에서 포션을 구매해서 사용해보자",500,50, false),
+            new Quest(4,"포션 아이템 사용","상점에서 포션을 구매해서 사용해보자",500,50, false),
         };
 
         //진행중인 퀘스트
@@ -75,25 +78,79 @@ namespace SpartaTextRPG.DataClass
         {
             int qeustNum = 1;
 
+            int row, col;
+
             //메인 퀘스트
-            Console.WriteLine("[메인 퀘스트]");
+            Color.ChangeTextColor(Colors.RED, "", "[메인 퀘스트]\n");
             foreach (Quest _quest in mainQuestList)
             {
                 if (_quest.isPossible)
                 {
-                    Console.WriteLine($"{qeustNum++}. {SortString(_quest.name, 15, 7)} | {_quest.info}|보상 : {_quest.gold} Gold, {_quest.exp} Exp");
+                    (row, col) = Console.GetCursorPosition();
+                    Console.Write($"{qeustNum++}.");
+                    Console.SetCursorPosition(3, col);
+                    Console.Write($"{_quest.name} ");
+                    Console.SetCursorPosition(18, col);
+                    Console.Write('|');
+
+                    Console.Write($" {_quest.info}");
+                    Console.SetCursorPosition(60, col);
+                    Console.Write('|');
+
+
+                    Console.Write($" {_quest.gold}".PadLeft(6));
+                    Console.SetCursorPosition(68, col);
+                    Console.Write("G");
+                    Console.SetCursorPosition(70, col);
+                    Console.Write('|');
+
+                    Console.Write($" {_quest.exp}".PadLeft(5));
+                    Console.SetCursorPosition(77, col);
+                    Console.Write("EXP ");
+                    Console.WriteLine("\n");
+
+                    //Console.WriteLine($"{qeustNum++}. {_quest.name} | {_quest.info}|보상 : {_quest.gold} Gold, {_quest.exp} Exp");
                 }
             }
 
-            Console.WriteLine("\n[서브 퀘스트]");
+            Color.ChangeTextColor(Colors.MAGENTA, "", "[서브 퀘스트]\n");
             //서브 퀘스트
             foreach (Quest _quest in subQuestList)
             {
                 if (_quest.isPossible)
                 {
-                    Console.WriteLine($"{qeustNum++}. {SortString(_quest.name, 15, 7)} | {_quest.info}|보상 : {_quest.gold} Gold, {_quest.exp} Exp");
+                    (row, col) = Console.GetCursorPosition();
+                    Console.Write($"{qeustNum++}.");
+                    Console.SetCursorPosition(3, col);
+                    Console.Write($"{_quest.name} ");
+                    Console.SetCursorPosition(18, col);
+                    Console.Write('|');
+
+                    Console.Write($" {_quest.info}");
+                    Console.SetCursorPosition(60, col);
+                    Console.Write('|');
+
+
+                    Console.Write($" {_quest.gold}".PadLeft(6));
+                    Console.SetCursorPosition(68, col);
+                    Console.Write("G");
+                    Console.SetCursorPosition(70, col);
+                    Console.Write('|');
+
+                    Console.Write($" {_quest.exp}".PadLeft(5));
+                    Console.SetCursorPosition(77, col);
+                    Console.Write("EXP ");
+                    Console.WriteLine();
+
+                    //Console.WriteLine($"{qeustNum++}. {_quest.name} | {_quest.info}|보상 : {_quest.gold} Gold, {_quest.exp} Exp");
                 }
             }
+        }
+
+        public int GetPadLenght(string _str)
+        {
+            int padLen = 30 - Encoding.Default.GetBytes(_str).Length;
+            return padLen;
         }
 
         //원하는 길이로 문자열 반환
@@ -103,7 +160,7 @@ namespace SpartaTextRPG.DataClass
 
             int addSpace = _maxLength - str.Length;
 
-            for (int i = _str.Length; i < _Lenght +addSpace; i++)
+            for (int i = _str.Length; i < _Lenght + addSpace; i++)
             {
                 str += " ";
             }
