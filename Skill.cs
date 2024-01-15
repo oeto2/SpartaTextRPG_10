7ba814833844ecda53e5c17d3af6cc2cc6657f8a
Jaeyoung Paik <100wodud@gmail.com>
2024-01-15 11:56:41 +0900

    0115_Add useSkill function

--- Skill.cs
+++ Skill.cs
@@ -36,15 +36,81 @@ namespace SpartaTextRPG
 
         //스킬 사용(원하는 스킬 인덱스 , 공격 몬스터 인덱스)
         //나중에 스플뎀 가능하면 넣을 예정
-        public static void useSkill(int skillnum, int monsternum, Monster[] monsters)
+        public static void useSkill(int skillnum,  List<Monster> monsters)
         {
-            float damage = Skills.myskills[skillnum].damage * (Player.player.baseAtk + Player.player.addAtk);
-            Math.Truncate(damage);
-            Player.player.hp += Skills.myskills[skillnum].hp;
-            Player.player.mp += Skills.myskills[skillnum].mp;
+            int monsternum;
+            bool res = false;
+            while (!res)
+            {
+                Console.WriteLine("공격할 몬스터를 선택해주세요.");
+                Console.Write(">>");
+                string userInput = Console.ReadLine();
+                int num;
 
-            Monster targetMonster = monsters[monsternum];
+                res = int.TryParse(userInput, out monsternum);
+
+                if (!res)
+                {
+                    Color.ChangeTextColor(Colors.RED, "", "잘못된 입력입니다.", "\n");
+                }
+                else if (monsters[monsternum - 1] != null)
+                {
+                    res = true;
+                    float damage = Skills.myskills[skillnum].damage * (Player.player.baseAtk + Player.player.addAtk);
+                    Math.Truncate(damage);
+                    Player.player.hp += Skills.myskills[skillnum].hp;
+                    Player.player.mp += Skills.myskills[skillnum].mp;
+
+                    monsters[monsternum - 1].Health -= damage;
+
+                }
+                else if (monsternum == 0)
+                {
+                    res = true;
+                }
+            }
             // Battle.OnDamage(targetMonster, damage);
+
+            Battle.instance.BattleScene();
+        }
+
+        public static void showSkill(List<Monster> monsters)
+        {
+            int i = 1;
+            foreach (var skill in Skills.myskills)
+            {
+                Color.ChangeTextColor(Colors.BLUE, "\n", $"{i}. {skill.name} ","");
+                if (skill.hp != 0)
+                {
+                    Console.Write($" - HP {-skill.hp} 사용\n");
+                }
+                if (skill.mp != 0)
+                {
+                    Console.Write($" - MP {-skill.mp} 사용\n");
+                }
+                i++;
+            }
+            bool res = false;
+            while (!res)
+            {
+                Console.WriteLine("행동을 선택해주세요.");
+                Console.Write(">>");
+                string userInput = Console.ReadLine();
+                int num;
+
+                res = int.TryParse(userInput, out num);
+
+                if (!res) 
+                {
+                    Color.ChangeTextColor(Colors.RED, "", "잘못된 입력입니다.", "\n");
+                }else if(Skills.myskills[num - 1] != null)
+                {
+                    useSkill(num - 1, monsters);
+                } else if(num  ==  0)
+                {
+                    Battle.instance.BattleScene();
+                }
+            }
         }
     }
 }
